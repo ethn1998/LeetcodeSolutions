b806ddad77e21799731cddb1f8e87e04cf176241
Eden Ng Tian Hwa <84511321+ethn1998@users.noreply.github.com>
2025-01-21 16:54:10 +0800

    Create 2169_Ops_To_Zero.py

---
+++ 2169_Ops_To_Zero.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def countOperations(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        fout = 0
+        while num1 > 0 and num2 > 0:
+            if num1 > num2:
+                num1 -= num2
+            else:
+                num2 -= num1
+            fout += 1
+        return fout
+        
