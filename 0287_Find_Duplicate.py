3491faff5f3b7b058c01feafc011ed9e9cf8b0d3
Eden Ng Tian Hwa <84511321+ethn1998@users.noreply.github.com>
2025-01-21 13:07:18 +0800

    Create 0287_Find_Duplicate.py

---
+++ 0287_Find_Duplicate.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        seen = set()
+        for n in nums:
+            if n in seen:
+                return n
+            else:
+                seen.add(n)
+        
