fc120a8fb58a181725ffe4b3bc8b71e463aca6cd
Eden Ng Tian Hwa <84511321+ethn1998@users.noreply.github.com>
2025-02-02 19:29:52 +0800

    Update 2124_Check_As_Before_Bs.py
    two pointer implementation.

--- 2124_Check_As_Before_Bs.py
+++ 2124_Check_As_Before_Bs.py
@@ -1,4 +1,9 @@
 class Solution(object):
+    """
+    STRATEGY 1:
+    detect 'b', and then detect 'a'
+    More robust, but could be slower in some cases
+    """
     def checkString(self, s):
         """
         :type s: str
@@ -12,4 +17,35 @@ class Solution(object):
                 if char == "a":
                     return False
         return True
+    """
+    STRATEGY 2:
+    two pointers: 
+    i: count 'a's from start and terminate at 'b'
+    j: count 'b's from end and terminate at 'a'
+    return i > j (first b appears after final a)
+    PROBLEM: Breaks if all entries are 'a' or 'b'
+    """
+
+    def checkStringij(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        b0 = True #Searching for first b
+        a1 = True #Searching for last a
+        i = 0
+        j = len(s)-1
+        while (b0 or a1) and (i<len(s)) and (j >= 0) :
+            if b0:
+                if s[i] == 'b':
+                    b0 = False
+                else:     
+                    i += 1    
+            if a1:
+                if s[j] == 'a':
+                    a1 = False
+                else:
+                    j -= 1
+        return ((b0 or a1) or (j < i))
+
         
