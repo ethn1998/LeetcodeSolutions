bb0e34169d0b6f384006adb9640f2b66932059eb
Eden Ng Tian Hwa <84511321+ethn1998@users.noreply.github.com>
2025-03-01 09:34:50 +0800

    Create 0283_MoveZeroes.go

---
+++ 0283_MoveZeroes.go
@@ -0,0 +1,13 @@
+func moveZeroes(nums []int)  {
+    n := len(nums)
+    for j := 0; j < len(nums); {
+        if nums[j] == 0 {
+            nums = append(nums[:j],nums[j+1:]...)
+        } else {
+            j++
+        }
+    }
+    for len(nums) < n {
+        nums = append(nums,0)
+    }
+}
