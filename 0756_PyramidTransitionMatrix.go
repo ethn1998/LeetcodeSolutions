46db1cec55e5786c475814018c9e6b954b63bb19
Eden Ng Tian Hwa <84511321+ethn1998@users.noreply.github.com>
2025-12-29 15:40:27 +0800

    Update 0756_PyramidTransitionMatrix.go
    BFS + queue memory to avoid duplicates + preprocess allowed vector for optimized lookup

--- 0756_PyramidTransitionMatrix.go
+++ 0756_PyramidTransitionMatrix.go
@@ -1,70 +1,46 @@
-/*
-IDEA: 
-We are basically constructing a recombining binary tree backwards.
-Solve recursively!
-*/
-
-//import fmt
-
 func pyramidTransition(bottom string, allowed []string) bool {
-	fmt.Println("Inputs:", bottom, allowed)
-	n := len(bottom) - 1 //length of next layer
-    if n <= 0 {
-        fmt.Println("CASE INPUT LETTER 0")
-        return true
+    //preprocess allowed
+    mem := make(map[string][]byte)
+    for _,word := range allowed{
+        key := word[0:2]
+        _, ok := mem[key]
+        if !ok {
+            mem[key] = []byte{word[2]}
+        } else {
+            mem[key] = append(mem[key],word[2])
+        }
     }
-	fmt.Println(n)
-
-	//generate all possible permutations of next layer
-	kids := make([]string, 0) //prelim
-	adults := make([]string, 0)
-
-	legs := bottom[0:2] // case where bottom has length <= 0 is already handled above.
-	//birth kids
-	for _, word := range allowed {
-		if word[0:2] == legs {
-			kids = append(kids, string(word[2]))
-		}
-	}
-	fmt.Println("kids:", kids)
-
-	if len(kids) == 0 {
-        fmt.Println("CASE CANNOT BUILD NEXT GEN")
-		return false
-	}
-	if n <= 1 {
-		fmt.Println("CASE INPUT LETTER1")
-		return true
-	} else {
-		//grow kids
-		var age int
-		for i := 0; i < len(kids); i++ {
-			kid := kids[i]
-			age = len(kid)
-			legs = bottom[age : age+2]
-			for _, word := range allowed {
-				//fmt.Println(word[0:2])
-				if word[0:2] == legs {
-					//fmt.Println("DEBUG INDICATOR")
-					if age == n-1 {
-						adults = append(adults, kid+string(word[2]))
-					} else {
-						kids = append(kids, kid+string(word[2]))
-					}
-				}
-			}
-			fmt.Println("age:", age, "kids:", kids)
-			fmt.Println("i:", i, "kids:", kids)
-		}
-	}
-	fmt.Println("adults:", adults)
-	var fout bool
-	for _, word := range adults {
-		if pyramidTransition(word, allowed) {
-			fout = true
-			break
-		}
-	}
-	//fmt.Println(fout)
-	return fout
+    
+    //DFS: Build from left to right
+    qmem := make(map[string]bool)
+    qmem[string(bottom[0])] = true
+    queue := []string{string(bottom[0])}
+    //mem := make(map[string]bool)
+    for i := 0; i < len(queue); i++ {
+        leaf := queue[i]
+        n := len(queue[i])
+        target := n + 1
+        kids := []string{string(bottom[n])}
+        for j := 0; j < len(kids); j++ {
+            kid := kids[j]
+            k := len(kid) - 1
+            //left := leaf[k]
+            //right := kid[k]
+            base := string(leaf[k]) + string(kid[k])
+            for _,char := range mem[base] {
+                next := kid + string(char)
+                    if len(next) < target {
+                        kids = append(kids,next)
+                    } else {
+                        if target == len(bottom){
+                            return true
+                        } else if !qmem[next]{
+                            queue = append(queue,next)
+                            qmem[next] = true
+                        }
+                    }
+                }
+            }
+        }
+    return false
 }
