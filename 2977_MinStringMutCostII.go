97cb916e331cc96de6a3fb690b2075190d8fae76
Eden Ng Tian Hwa <84511321+ethn1998@users.noreply.github.com>
2026-01-30 19:30:54 +0800

    Update 2977_MinStringMutCostII.go
    Trie implementation + upgraded querying process to avoid duplicated calculations

--- 2977_MinStringMutCostII.go
+++ 2977_MinStringMutCostII.go
@@ -1,79 +1,128 @@
+type Trie struct {
+	child [26]*Trie //array of capacity 26, size of alphabet
+	id    int
+}
+
+func newTrie() *Trie {
+	return &Trie{id: -1}
+}
+
+func add(node *Trie, word string, idx *int) int { //inserts new word into trie
+	for _, char := range word {
+		i := char - 'a'
+		if node.child[i] == nil {
+			node.child[i] = newTrie()
+		}
+		node = node.child[i]
+	}
+	if node.id == -1 { //set unique id (key) for each word in trie
+		*idx++
+		node.id = *idx
+	}
+	return node.id //return unique id of new word
+}
+
+/*
+func update(x *int64, y int64) { //DO WE NEED THIS?!
+    if x == -1 || y < *x {
+        *x = y
+    }
+}
+*/
+
 func minimumCost(source string, target string, original []string, changed []string, cost []int) int64 {
-    //fmt.Println(len(source),len(cost))//DEBUG
-	var src, trans, dst string
+	//fmt.Println(len(source),len(cost))//DEBUG
+	//var src, trans, dst string
+	var isrc, curr, itrans, idst int
 	var srctotrans, transtodst int64
 
-	costmat := make(map[string]map[string]int64, len(original))
-	for _, src = range original {
-		_, ok := costmat[src]
+	n := len(source)   //length of string
+	m := len(original) //2*m = max number of nodes in graph
+
+	root := newTrie() //Trie for encrypting substrings
+	id := -1          //init counter for id
+
+	//costmat := make(map[string]map[string]int64, len(original))
+	costmat := make(map[int]map[int]int64, len(original))
+	for _, src := range original {
+		isrc = add(root, src, &id)
+		_, ok := costmat[isrc]
 		if !ok {
-			costmat[src] = make(map[string]int64)
-			//costmat[src][src] = 0 //redundant?
-			queue := []string{src}
+			costmat[isrc] = make(map[int]int64)
+			costmat[isrc][isrc] = 0 //redundant?
+			queue := []int{isrc}
 			for i := 0; i < len(queue); i++ {
-				trans = queue[i]
-				srctotrans = costmat[src][trans]
-				for j := 0; j < len(original); j++ {
-					if original[j] == trans {
-						dst = changed[j]
+				curr = queue[i]
+				srctotrans = costmat[isrc][curr]
+				for j := 0; j < m; j++ {
+					itrans = add(root, original[j], &id)
+					if itrans == curr {
+						idst = add(root, changed[j], &id)
 						transtodst = int64(cost[j])
-						_, ok := costmat[src][dst]
+						_, ok := costmat[isrc][idst]
 						if ok {
-							if costmat[src][dst] > srctotrans+transtodst {
-								costmat[src][dst] = srctotrans + transtodst
-								queue = append(queue, dst)
+							if costmat[isrc][idst] > srctotrans + transtodst {
+								costmat[isrc][idst] = srctotrans + transtodst
+								queue = append(queue, idst)
 							}
 						} else {
-							costmat[src][dst] = srctotrans + transtodst
-							queue = append(queue, dst)
+							costmat[isrc][idst] = srctotrans + transtodst
+							queue = append(queue, idst)
 						}
 					}
 				}
 			}
 		}
 	}
-	/*DEBUG
-	for key, val := range costmat {
-		fmt.Println(key, ":", val)
-	}
-	check := make([]bool, len(source)+1) //redundancy for verification
-	*/
-	dp := make([]int64, len(source)+1) //cost to mutate first 'right' elements to target
-	var prev int64
-	for right := 1; right < len(dp); right++ {
-		//fmt.Println("In:", right, source[0:right], target[0:right]) //DEBUG
-		dp[right] = -1 //default output for nonexistent solution
-		if source[right-1] == target[right-1] {
-            dp[right] = dp[right-1]
-        }        
-        for left := 0; left < right; left++ { //NOTE: THIS IMPLEMENTATION IS INEFFICIENT
-			//sanity check if partial solution exists
-            prev = dp[left]
-            //fmt.Println("idx:",left,prev) //DEBUG
-			if prev > -1 { //only proceed if operation is valid
-				src = source[left:right]
-				dst = target[left:right]
-				dsts, ok := costmat[src]
-                //fmt.Println(src,dst,dsts,ok) //DEBUG
-				if ok {
-					val, ok := dsts[dst]
-					if ok {
-						if dp[right] > -1 {
-							dp[right] = min(dp[right], prev+val)
-						} else {
-							dp[right] = prev + val
-                            //check[right] = true //DEBUG
-						}
-					}
-				}
-			}
-			//fmt.Println("Out:", dp[right], check[right]) //DEBUG
-		}
-		//fmt.Println("Final Out:", right, source[0:right], target[0:right], dp[right], check[right]) //DEBUG
-	}
-    //DEBUG
-	//fmt.Println(dp) 
-	//fmt.Println(check)
-	//
-    return dp[len(source)]
+
+    dp := make([]int64,n)
+    for i := range dp {
+        dp[i] = -1
+    }
+
+    for left := 0; left < n; left++ {
+        if left > 0 { //to avoid indexing errors, and only trigger after we init left = 0 case
+            if dp[left - 1] == -1 {
+                continue
+            }
+        }
+        var prev int64
+        if left == 0 {
+            prev = 0
+        } else {
+            prev = dp[left-1]
+        }
+        if source[left] == target[left] {
+            if dp[left] > -1 {
+                if dp[left] > prev {
+                    dp[left] = prev
+                }
+            } else {
+                dp[left] = prev
+            }
+        }
+        u := root
+        v := root
+        for right := left; right < n; right++ { //try to convert string[left:right]
+            u = u.child[source[right]-'a']
+            v = v.child[target[right]-'a']
+            if u == nil || v == nil {
+                break
+            }
+            if u.id != -1 && v.id != -1 {
+                val, ok := costmat[u.id][v.id]
+                if ok {
+                    if dp[right] > -1 {
+                        if dp[right] > prev + val {
+                            dp[right] = prev + val
+                        }
+                    } else {
+                        dp[right] = prev + val
+                    }
+                }
+            }
+        }
+    }
+	//fmt.Println(dp) //DEBUG
+	return dp[len(source)-1]
 }
